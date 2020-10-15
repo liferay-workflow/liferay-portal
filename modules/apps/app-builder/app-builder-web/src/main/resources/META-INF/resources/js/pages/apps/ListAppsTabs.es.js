cba461fd052d7b318218073dd36e02a6e8cb643e
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-10-15 16:42:43 -0300

    LPS-116986 Fix list apps back navigation

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListAppsTabs.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListAppsTabs.es.js
@@ -13,7 +13,6 @@
  */
 
 import React, {useContext} from 'react';
-import {Redirect} from 'react-router-dom';
 
 import {AppContext} from '../../AppContext.es';
 import ControlMenu from '../../components/control-menu/ControlMenu.es';
@@ -22,13 +21,7 @@ import useLazy from '../../hooks/useLazy.es';
 
 export default (props) => {
 	const {appsTabs, appsTabsKeys} = useContext(AppContext);
-	const {tab} = props.match.params;
-
-	if (!tab || !appsTabsKeys.includes(tab)) {
-		const initialTabKey = appsTabsKeys[0] || 'standard';
-
-		return <Redirect to={`/${initialTabKey}`} />;
-	}
+	const {tab = appsTabsKeys[0]} = props.match.params;
 
 	const {listEntryPoint, ...otherProps} = appsTabs[tab];
 	const navTabs = Object.values(appsTabs).map(({label, scope}) => ({
