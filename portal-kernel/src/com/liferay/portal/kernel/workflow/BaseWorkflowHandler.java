b1c91dcf7b5fe51ccea7679e5f19cd14e5cb1d07
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-03-18 14:41:22 -0300

    LPS-127391 Create redirect URL to preview mode when the entry was accessed from notification

--- portal-kernel/src/com/liferay/portal/kernel/workflow/BaseWorkflowHandler.java
+++ portal-kernel/src/com/liferay/portal/kernel/workflow/BaseWorkflowHandler.java
@@ -172,6 +172,28 @@ public abstract class BaseWorkflowHandler<T> implements WorkflowHandler<T> {
 	}
 
 	@Override
+	public String getURLNotificationLink(
+			long workflowTaskId, ServiceContext serviceContext)
+		throws PortalException {
+
+		try {
+			PortletURL portletURL = PortletURLFactoryUtil.create(
+				serviceContext.getRequest(), PortletKeys.MY_WORKFLOW_TASK,
+				PortletRequest.RENDER_PHASE);
+
+			portletURL.setParameter("mvcPath", "/edit_workflow_task.jsp");
+			portletURL.setParameter(
+				"workflowTaskId", String.valueOf(workflowTaskId));
+			portletURL.setWindowState(WindowState.MAXIMIZED);
+
+			return portletURL.toString();
+		}
+		catch (WindowStateException windowStateException) {
+			throw new PortalException(windowStateException);
+		}
+	}
+
+	@Override
 	public PortletURL getURLViewDiffs(
 		long classPK, LiferayPortletRequest liferayPortletRequest,
 		LiferayPortletResponse liferayPortletResponse) {
