06282533a1dafd883562325f27c1d0be3ee38e8c
Rafael Praxedes <rafael.praxedes@liferay.com>
2021-03-22 12:11:38 -0300

    LPS-129410 SF

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/util/CalendarICalDataHandler.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/util/CalendarICalDataHandler.java
@@ -676,7 +676,7 @@ public class CalendarICalDataHandler implements CalendarDataHandler {
 		Company company = CompanyLocalServiceUtil.getCompany(
 			calendarBooking.getCompanyId());
 
-		String calendarBookingDescriptionText = StringUtil.replace(
+		String calendarBookingDescription = StringUtil.replace(
 			calendarBooking.getDescription(user.getLocale()),
 			new String[] {"href=\"/", "src=\"/"},
 			new String[] {
@@ -686,15 +686,13 @@ public class CalendarICalDataHandler implements CalendarDataHandler {
 					"/"
 			});
 
-		String calendarBookingDescription = HtmlUtil.stripHtml(
-			calendarBookingDescriptionText);
-
-		Description description = new Description(calendarBookingDescription);
+		Description description = new Description(
+			HtmlUtil.stripHtml(calendarBookingDescription));
 
 		propertyList.add(description);
 
 		XProperty xProperty = new XProperty(
-			"X-ALT-DESC", calendarBookingDescriptionText);
+			"X-ALT-DESC", calendarBookingDescription);
 
 		ParameterList parameters = xProperty.getParameters();
 
