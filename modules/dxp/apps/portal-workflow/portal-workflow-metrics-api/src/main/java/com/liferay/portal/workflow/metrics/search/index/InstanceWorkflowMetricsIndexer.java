e7cc08c8166bbbf5104d5fd5cc3f36246ad86b48
kiwm <pedro.malta@liferay.com>
2022-11-04 13:55:31 -0300

    LPS-146859 Add createDefaultDocument method to instance indexer, add name index and type index getters

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/InstanceWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/InstanceWorkflowMetricsIndexer.java
@@ -35,7 +35,16 @@ public interface InstanceWorkflowMetricsIndexer {
 		long companyId, Date completionDate, long duration, long instanceId,
 		Date modifiedDate);
 
+	public Document createDefaultDocument(
+		boolean active, Map<Locale, String> assetTitleMap,
+		Map<Locale, String> assetTypeMap, long companyId, long instanceId,
+		Date modifiedDate);
+
 	public void deleteInstance(long companyId, long instanceId);
+
+	public String getIndexName(long companyId);
+
+	public String getIndexType();
 
 	public Document updateInstance(
 		boolean active, Map<Locale, String> assetTitleMap,
