7732bf53998b58ae6dd43111fda626e35f00703c
Nara Andrade <nara.andrade@liferay.com>
2023-04-10 15:54:26 -0300

    LPS-177901 Use name as the identifier for nodes and transitions according with the xsd. Keep using id only for the react flow elements.

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/NodeInformation.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/NodeInformation.js
@@ -86,18 +86,18 @@ export default function NodeInformation({errors, setErrors}) {
 						: ''
 				}
 			>
-				<label htmlFor="nodeId">
+				<label htmlFor="nodeName">
 					<span>
 						{`${Liferay.Language.get(
 							'node'
-						)} ${Liferay.Language.get('id')}`}
+						)} ${Liferay.Language.get('name')}`}
 					</span>
 
 					<span className="ml-1 mr-1 text-warning">*</span>
 
 					<span
 						title={Liferay.Language.get(
-							'id-is-the-node-identifier'
+							'name-is-the-node-identifier'
 						)}
 					>
 						<ClayIcon
@@ -108,13 +108,13 @@ export default function NodeInformation({errors, setErrors}) {
 				</label>
 
 				<ClayInput
-					id="nodeId"
+					id="nodeName"
 					onChange={({target}) => {
 						const filteredElements = elements.slice();
 
 						filteredElements.splice(
 							elements.findIndex(
-								(element) => element.id === selectedItem.id
+								(element) => element.name === selectedItem.name
 							),
 							1
 						);
@@ -135,7 +135,7 @@ export default function NodeInformation({errors, setErrors}) {
 
 							{errors.id.duplicated
 								? Liferay.Language.get(
-										'a-node-with-that-id-already-exists'
+										'a-node-with-that-name-already-exists'
 								  )
 								: Liferay.Language.get(
 										'this-field-is-required'
