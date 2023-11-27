9a94a7f67ea431a6e521bea6330b960f881e23ba
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-11-27 13:40:33 -0300

    LPS-200952 Apply WorkflowMetricsReindexMessageListener to use

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/creation/instance/lifecycle/WorkflowMetricsIndexInitialRequestPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/creation/instance/lifecycle/WorkflowMetricsIndexInitialRequestPortalInstanceLifecycleListener.java
@@ -5,11 +5,22 @@
 
 package com.liferay.portal.workflow.metrics.internal.search.index.creation.instance.lifecycle;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.instance.lifecycle.InitialRequestPortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.workflow.metrics.constants.WorkflowMetricsDestinationNames;
 import com.liferay.portal.workflow.metrics.internal.search.index.creation.helper.WorkflowMetricsIndexCreator;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -22,8 +33,32 @@ public class WorkflowMetricsIndexInitialRequestPortalInstanceLifecycleListener
 	extends InitialRequestPortalInstanceLifecycleListener {
 
 	@Override
-	public void portalInstancePreregistered(Company company) throws Exception {
+	public void doPortalInstanceRegistered(long companyId) throws Exception {
+		Company company = _companyLocalService.getCompany(companyId);
+
 		_workflowMetricsIndexCreator.createIndex(company);
+
+		String jobName = StringBundler.concat(
+			WorkflowMetricsDestinationNames.WORKFLOW_METRICS_REINDEX,
+			StringPool.SLASH, company.getCompanyId());
+
+		SchedulerResponse schedulerResponse =
+			_schedulerEngineHelper.getScheduledJob(
+				jobName, jobName, StorageType.MEMORY);
+
+		if (schedulerResponse != null) {
+			return;
+		}
+
+		Message message = new Message();
+
+		message.put("companyId", company.getCompanyId());
+
+		_schedulerEngineHelper.schedule(
+			_triggerFactory.createTrigger(
+				jobName, jobName, new Date(), null, 5, TimeUnit.SECOND),
+			StorageType.MEMORY, null,
+			WorkflowMetricsDestinationNames.WORKFLOW_METRICS_REINDEX, message);
 	}
 
 	@Override
@@ -31,14 +66,14 @@ public class WorkflowMetricsIndexInitialRequestPortalInstanceLifecycleListener
 		_workflowMetricsIndexCreator.removeIndex(company);
 	}
 
-	@Override
-	protected void doPortalInstanceRegistered(long companyId) throws Exception {
-		_workflowMetricsIndexCreator.reindex(
-			_companyLocalService.getCompany(companyId));
-	}
-
 	@Reference
 	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private SchedulerEngineHelper _schedulerEngineHelper;
+
+	@Reference
+	private TriggerFactory _triggerFactory;
 
 	@Reference
 	private WorkflowMetricsIndexCreator _workflowMetricsIndexCreator;
