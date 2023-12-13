45f42b3fe8a52ae11ae58e3412b9ad147ff1c114
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-12-13 12:41:15 -0300

    LPS-200952 Handle ConstraintViolationException - Not need to add af it's already persisted

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/util/RoleUtil.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/util/RoleUtil.java
@@ -12,6 +12,7 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.DuplicateRoleException;
 import com.liferay.portal.kernel.exception.NoSuchRoleException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.service.RoleLocalService;
@@ -67,9 +68,24 @@ public class RoleUtil {
 				role = accountRole.getRole();
 			}
 			else {
-				role = roleLocalService.addRole(
-					serviceContext.getUserId(), null, 0, name, null,
-					descriptionMap, roleType, null, null);
+				try {
+					role = roleLocalService.addRole(
+						serviceContext.getUserId(), null, 0, name, null,
+						descriptionMap, roleType, null, null);
+				}
+				catch (SystemException systemException) {
+					role = roleLocalService.fetchRole(
+						serviceContext.getCompanyId(), name);
+
+					if (role == null) {
+						throw systemException;
+					}
+
+					if (role.getType() != roleType) {
+						throw new DuplicateRoleException(
+							"Role already exists with name " + name);
+					}
+				}
 			}
 		}
 		else if (role.getType() != roleType) {
