660d0456d22e957d9aefdd07680ec257973ded52
Selton Guedes <selton.guedes@liferay.com>
2024-02-22 16:59:55 -0300

    LPD-16334 Use KaleoDefinitionVersionService instead of KaleoDefinitionVersionLocalService

---
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoDefinitionVersionServiceImpl.java
@@ -0,0 +1,79 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.workflow.kaleo.service.impl;
+
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
+import com.liferay.portal.workflow.kaleo.service.base.KaleoDefinitionVersionServiceBaseImpl;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Selton Guedes
+ */
+@Component(
+	property = {
+		"json.web.service.context.name=kaleo",
+		"json.web.service.context.path=KaleoDefinitionVersion"
+	},
+	service = AopService.class
+)
+public class KaleoDefinitionVersionServiceImpl
+	extends KaleoDefinitionVersionServiceBaseImpl {
+
+	@Override
+	public KaleoDefinitionVersion getKaleoDefinitionVersion(
+			long companyId, String name, String version)
+		throws PortalException {
+
+		KaleoDefinitionVersion kaleoDefinitionVersion =
+			_kaleoDefinitionVersionLocalService.getKaleoDefinitionVersion(
+				companyId, name, version);
+
+		_kaleoDefinitionVersionModelResourcePermission.check(
+			getPermissionChecker(), kaleoDefinitionVersion, ActionKeys.VIEW);
+
+		return kaleoDefinitionVersion;
+	}
+
+	@Override
+	public List<KaleoDefinitionVersion> getKaleoDefinitionVersions(
+			long companyId, String name)
+		throws PortalException {
+
+		List<KaleoDefinitionVersion> kaleoDefinitionVersions =
+			_kaleoDefinitionVersionLocalService.getKaleoDefinitionVersions(
+				companyId, name);
+
+		if (kaleoDefinitionVersions.isEmpty()) {
+			return kaleoDefinitionVersions;
+		}
+
+		_kaleoDefinitionVersionModelResourcePermission.check(
+			getPermissionChecker(), kaleoDefinitionVersions.get(0),
+			ActionKeys.VIEW);
+
+		return kaleoDefinitionVersions;
+	}
+
+	@Reference
+	private KaleoDefinitionVersionLocalService
+		_kaleoDefinitionVersionLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion)"
+	)
+	private ModelResourcePermission<KaleoDefinitionVersion>
+		_kaleoDefinitionVersionModelResourcePermission;
+
+}
