d79a9a034788084d7fa69341c9a9f9c57be3c889
Richard Jeremias <richard.jeremias@liferay.com>
2024-07-10 17:58:29 -0300

    LPD-28216 Ensure non assigned users can only read the tasks

--- modules/test/playwright/pages/message-boards/MessageBoardsPage.ts
+++ modules/test/playwright/pages/message-boards/MessageBoardsPage.ts
@@ -11,11 +11,14 @@ import {PORTLET_URLS} from '../../utils/portletUrls';
 export class MessageBoardsPage {
 	readonly actionAddMessage: Locator;
 	readonly actionReplyMessage: Locator;
+	readonly deleteAllMBEntriesButton: Locator;
 	readonly homeCategoryPermissionsFrame: FrameLocator;
 	readonly homeCategoryPermissionsMenuItem: Locator;
+	readonly newThreadButton: Locator;
 	readonly page: Page;
 	readonly optionsMenu: Locator;
 	readonly saveButton: Locator;
+	readonly selectAllMBEntriesCheckBox: Locator;
 
 	constructor(page: Page) {
 		this.homeCategoryPermissionsFrame = page.frameLocator(
@@ -28,15 +31,27 @@ export class MessageBoardsPage {
 		this.actionReplyMessage = this.homeCategoryPermissionsFrame.locator(
 			'#guest_ACTION_REPLY_TO_MESSAGE'
 		);
+		this.deleteAllMBEntriesButton = page.getByRole('button', {
+			name: 'Delete',
+		});
 		this.homeCategoryPermissionsMenuItem = page.getByRole('menuitem', {
 			name: 'Home Category Permissions',
 		});
+		this.newThreadButton = page.getByRole('link', {name: 'New Thread'});
 		this.page = page;
 		this.optionsMenu = page.getByLabel('Options');
 		this.saveButton = this.homeCategoryPermissionsFrame.getByRole(
 			'button',
 			{name: 'Save'}
 		);
+		this.selectAllMBEntriesCheckBox = page.getByLabel(
+			'Select All Items on the Page'
+		);
+	}
+
+	async deleteAllMBEntries() {
+		await this.selectAllMBEntriesCheckBox.check();
+		await this.deleteAllMBEntriesButton.click();
 	}
 
 	async goto(siteUrl?: Site['friendlyUrlPath']) {
@@ -61,6 +76,35 @@ export class MessageBoardsPage {
 
 		await this.actionAddMessage.check();
 		await this.actionReplyMessage.check();
+
+		await this.saveButton.click();
+
+		await this.page.getByLabel('close', {exact: true}).click();
+	}
+
+	async setRoleCategoryPermissions(
+		roleName: string,
+		siteUrl?: Site['friendlyUrlPath']
+	) {
+		await this.goto(siteUrl);
+
+		await this.optionsMenu.click();
+		await this.homeCategoryPermissionsMenuItem.click();
+
+		await this.homeCategoryPermissionsFrame
+			.locator(`#${roleName}_ACTION_ADD_MESSAGE`)
+			.first()
+			.check();
+
+		await this.homeCategoryPermissionsFrame
+			.locator(`#${roleName}_ACTION_REPLY_TO_MESSAGE`)
+			.first()
+			.check();
+
+		await this.homeCategoryPermissionsFrame
+			.locator(`#${roleName}_ACTION_VIEW`)
+			.first()
+			.check();
 
 		await this.saveButton.click();
 
